520ffb04b15395ff0b9880ac7ddae1e7982d19d6
khattab88 <m_khatab_88@hotmail.com>
2021-08-10 23:19:20 +0200

    remove old code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
-import { TestComponent } from './test/test.component';
-
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'form', component: HeroFormComponent },
-  { path: 'test', component: TestComponent }
 ];
 
 @NgModule({
