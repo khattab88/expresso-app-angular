89093c29483410257b5bba1cc8dcb7d8c6855d25
khattab88 <m_khatab_88@hotmail.com>
2021-08-11 01:44:27 +0200

    create recipe app folder

---
+++ src/app/angular-course/recipe-app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recipe-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class RecipeHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { RecipeHeaderComponent } from './angular-course/recipe-app/header/header.component';
+import { RecipeAppComponent } from './angular-course/recipe-app/app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
+    RecipeHeaderComponent,
+    RecipeAppComponent,
   ],
   imports: [
     BrowserModule,
