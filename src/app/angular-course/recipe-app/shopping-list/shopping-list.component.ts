8595ad0e97e311b6fe43bda381e390d7acb667a6
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-08-11 11:45:45 +0200

    update recipe shopping list components

--- src/app/angular-course/recipe-app/shopping-list/shopping-list.component.ts
+++ src/app/angular-course/recipe-app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/Ingredient.model';
+
 @Component({
   selector: 'recipe-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class RecipeShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.ingredients = [
+      new Ingredient("ingredient 1", 1),
+      new Ingredient("ingredient 2", 3),
+      new Ingredient("ingredient 3", 2)
+    ];
   }
 
 }
