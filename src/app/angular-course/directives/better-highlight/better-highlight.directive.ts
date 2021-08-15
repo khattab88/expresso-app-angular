e86e07bfed7ece06965e0ea71804b043a8e8ae93
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-08-15 13:06:05 +0200

    better highlight directive - react to mouse events

--- src/app/angular-course/directives/better-highlight/better-highlight.directive.ts
+++ src/app/angular-course/directives/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,6 +8,15 @@ export class BetterHighlightDirective {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "limegreen");
+    this.renderer.setStyle(this.elementRef.nativeElement, "cursor", "pointer");
+  }
+
+  @HostListener('mouseenter') mouseover(e: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "limegreen");
+  }
+
+  @HostListener('mouseleave') mouseleave(e: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, "background-color", "transparent");
   }
 }
