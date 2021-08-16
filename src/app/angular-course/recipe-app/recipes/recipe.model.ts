5f092dc766d979a005c125053c7b2a27e5b3d85d
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-08-16 11:22:43 +0200

    update recipe model (add ingredients field)

--- src/app/angular-course/recipe-app/recipes/recipe.model.ts
+++ src/app/angular-course/recipe-app/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from "../shared/Ingredient.model";
+
 export class Recipe {
     // id: number;
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name: string, desc: string, image: string) {
+    constructor(name: string, desc: string, image: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = image;
+        this.ingredients = ingredients;
     }
 }
--- src/app/angular-course/recipe-app/shared/services/recipe.service.ts
+++ src/app/angular-course/recipe-app/shared/services/recipe.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { Recipe } from '../../recipes/recipe.model';
+import { Ingredient } from '../Ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,25 @@ export class RecipeService {
   constructor() {
     this.recipes = [
       new Recipe("Recipe 1", "this is recipe 1",
-        "https://p1.pxfuel.com/preview/4/17/111/recipe-comisa-healthy-tomato.jpg"),
+        "https://p1.pxfuel.com/preview/4/17/111/recipe-comisa-healthy-tomato.jpg",
+        [
+          new Ingredient("bread", 1),
+          new Ingredient("salt", 2),
+          new Ingredient("meat", 1)
+        ]),
       new Recipe("Recipe 2", "this is recipe 2",
-        "https://i0.hippopx.com/photos/195/737/99/spring-roll-spring-roll-materials-spring-roll-ingredient-materials-preview.jpg"),
+        "https://i0.hippopx.com/photos/195/737/99/spring-roll-spring-roll-materials-spring-roll-ingredient-materials-preview.jpg",
+        [
+          new Ingredient("bread", 1),
+          new Ingredient("salt", 2),
+          new Ingredient("fish", 3)
+        ]),
       new Recipe("Recipe 3", "this is recipe 3",
-        "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1.jpg")
+        "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1.jpg",
+        [
+          new Ingredient("bread", 1),
+          new Ingredient("salt", 2)
+        ])
     ];
   }
 
@@ -25,9 +40,9 @@ export class RecipeService {
     return of(this.recipes);
   }
 
-  
+
   addRecipe(recipe: Recipe): void {
-    if(!recipe) return;
+    if (!recipe) return;
 
     this.recipes.push(recipe);
   }
