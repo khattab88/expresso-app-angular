6660f311e7274ad4258348e0ef6f32fd4ae11c2d
mahmoud.khattab <mahmoud.khattab@MAHMOUD-KHATTAB>
2021-08-19 17:20:29 +0200

    bind template from to angular forms system

--- src/app/angular-course/forms/template-form/template-form.component.ts
+++ src/app/angular-course/forms/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'template-form',
@@ -10,6 +11,14 @@ export class TemplateFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  suggestName(): string {
+    return "SuperName";
+  }
+
+  onSubmit(userForm: NgForm) {
+    console.log(userForm);
   }
 
 }
